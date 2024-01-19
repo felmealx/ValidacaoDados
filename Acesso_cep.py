47ef4444b50c16048904fec6112c455241ad9a65
felipe <felipe.m.alexandrino@gmail.com>
2024-01-18 21:52:55 -0300

    Respostas API

--- Acesso_cep.py
+++ Acesso_cep.py
@@ -1,3 +1,4 @@
+import requests
 class BuscaEndereco:
 
     def __init__(self,cep):
@@ -11,8 +12,17 @@ class BuscaEndereco:
             return True
         else:
             return False
-
     def __str__(self):
         return self.format_cep()
     def format_cep(self):
         return "{}-{}".format(self.cep[:5],self.cep[5:])
+
+    def acessa_via_cep(self):
+        url = 'https://viacep.com.br/ws/{}/json/'.format(self.cep)
+        r = requests.get(url)
+        dados = r.json()
+        return (
+            dados['bairro'],
+            dados['localidade'],
+            dados['uf']
+        )
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+
 from Acesso_cep import BuscaEndereco
 
 cep = "04181160"
 
 obj = BuscaEndereco(cep)
 
-print(obj)
+bairro,cidade,uf = obj.acessa_via_cep()
+
+print(bairro,cidade,uf)
 
